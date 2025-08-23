0193fa94f588f69fdb89e4a42a0c3bc1171d2c4f
fzh200001 <123788384+fzh200001@users.noreply.github.com>
2025-08-23 18:42:27 +0800

    Update pubsFromBib.py

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -28,12 +28,16 @@ import re
 publist = {
     "proceeding": {
         "file" : "proceedings.bib",
+        "venuekey": "booktitle",
+        "venue-pretext": "In the proceedings of ",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
         
     },
     "journal":{
         "file": "pubs.bib",
+        "venuekey" : "journal",
+        "venue-pretext" : "",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
     } 
@@ -53,9 +57,59 @@ def html_escape(text):
 for pubsource in publist:
     parser = bibtex.Parser()
     bibdata = parser.parse_file(publist[pubsource]["file"])
+
+    #loop through the individual references in a given bibtex file
+    for bib_id in bibdata.entries:
+        #reset default date
+        pub_year = "1900"
+        pub_month = "01"
+        pub_day = "01"
+        
+        b = bibdata.entries[bib_id].fields
+        
+        try:
+            pub_year = f'{b["year"]}'
+
+            #todo: this hack for month and day needs some cleanup
+            if "month" in b.keys(): 
+                if(len(b["month"])<3):
+                    pub_month = "0"+b["month"]
+                    pub_month = pub_month[-2:]
+                elif(b["month"] not in range(12)):
+                    tmnth = strptime(b["month"][:3],'%b').tm_mon   
+                    pub_month = "{:02d}".format(tmnth) 
+                else:
+                    pub_month = str(b["month"])
+            if "day" in b.keys(): 
+                pub_day = str(b["day"])
+
+                
+            pub_date = pub_year+"-"+pub_month+"-"+pub_day
             
             #strip out {} as needed (some bibtex entries that maintain formatting)
-            clean_title = b["title"].replace("{", "").replace("}","").replace("\\","").replace(" ","-")   
+            clean_title = b["title"].replace("{", "").replace("}","").replace("\\","").replace(" ","-")    
+
+            url_slug = re.sub("\\[.*\\]|[^a-zA-Z0-9_-]", "", clean_title)
+            url_slug = url_slug.replace("--","-")
+
+            md_filename = (str(pub_date) + "-" + url_slug + ".md").replace("--","-")
+            html_filename = (str(pub_date) + "-" + url_slug).replace("--","-")
+
+            #Build Citation from text
+            citation = ""
+
+            #citation authors - todo - add highlighting for primary author?
+            for author in bibdata.entries[bib_id].persons["author"]:
+                citation = citation+" "+author.first_names[0]+" "+author.last_names[0]+", "
+
+            #citation title
+            citation = citation + "\"" + html_escape(b["title"].replace("{", "").replace("}","").replace("\\","")) + ".\""
+
+            #add venue logic depending on citation type
+            venue = publist[pubsource]["venue-pretext"]+b[publist[pubsource]["venuekey"]].replace("{", "").replace("}","").replace("\\","")
+
+            citation = citation + " " + html_escape(venue)
+            citation = citation + ", " + pub_year + "."
 
             
             ## YAML variables
@@ -70,6 +124,10 @@ for pubsource in publist:
                 if len(str(b["note"])) > 5:
                     md += "\nexcerpt: '" + html_escape(b["note"]) + "'"
                     note = True
+
+            md += "\ndate: " + str(pub_date) 
+
+            md += "\nvenue: '" + html_escape(venue) + "'"
             
             url = False
             if "url" in b.keys():
@@ -77,6 +135,7 @@ for pubsource in publist:
                     md += "\npaperurl: '" + b["url"] + "'"
                     url = True
 
+            md += "\ncitation: '" + html_escape(citation) + "'"
 
             md += "\n---"
 
