eb52d165b1271426a7197278343573ed32fc208d
fzh200001 <123788384+fzh200001@users.noreply.github.com>
2025-08-23 18:39:10 +0800

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -64,6 +64,10 @@ def html_escape(text):
 import os
 for row, item in publications.iterrows():
     
+    md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
+    html_filename = str(item.pub_date) + "-" + item.url_slug
+    year = item.pub_date[:4]
+    
     ## YAML variables
     
     md = "---\ntitle: \""   + item.title + '"\n'
@@ -78,9 +82,12 @@ for row, item in publications.iterrows():
     
     md += "\ndate: " + str(item.pub_date) 
     
+    md += "\nvenue: '" + html_escape(item.venue) + "'"
+    
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
+    md += "\ncitation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
     
@@ -91,10 +98,11 @@ for row, item in publications.iterrows():
         
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
+        
+    md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
     with open("../_publications/" + md_filename, 'w') as f:
         f.write(md)
-
 
